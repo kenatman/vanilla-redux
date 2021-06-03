e5089d6e94d3bb6953a8e315df0167c122706b8e
Yunsu <kenatman0723@gmail.com>
2021-06-03 18:00:07 +0900

    createStore & reducer : Reducer is a function that modifies your data

--- src/index.js
+++ src/index.js
@@ -1,23 +1,13 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let counter = 0;
-
-number.innerText = counter;
-
-const update = () => {
-  number.innerText = counter;
+const countModifier = (count = 0) => {
+  return count;
 };
 
-const handleAdd = () => {
-  counter = counter + 1;
-  update();
-};
-const handleMinus = () => {
-  counter = counter - 1;
-  update();
-};
+const countStore = createStore(countModifier);
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
