7a12bee14494c831f1f06831780ec74c579c743b
Yunsu <kenatman0723@gmail.com>
2021-06-04 17:10:17 +0900

    redux refactoring

--- src/index.js
+++ src/index.js
@@ -6,23 +6,26 @@ const number = document.querySelector("span");
 
 number.innerText = 0;
 
+const selection = Object.freeze({ add: `ADD`, minus: `MINUS` });
+
 const countModifier = (count = 0, action) => {
-  if (action.type === `ADD`) {
-    return count + 1;
-  } else if (action.type === `MINUS`) {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case selection.add:
+      return count + 1;
+    case selection.minus:
+      return count - 1;
+    default:
+      return count;
   }
 };
 
 const countStore = createStore(countModifier);
 
 add.addEventListener("click", () => {
-  countStore.dispatch({ type: `ADD` });
+  countStore.dispatch({ type: selection.add });
 });
 minus.addEventListener("click", () => {
-  countStore.dispatch({ type: `MINUS` });
+  countStore.dispatch({ type: selection.minus });
 });
 
 countStore.subscribe(() => {
