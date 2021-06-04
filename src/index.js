9feb4cce65ff044a68080311eb96c540024628ab
Yunsu <kenatman0723@gmail.com>
2021-06-04 16:57:46 +0900

    Store.subscribe(cb) : allows us to listen to changes in store

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
+number.innerText = 0;
+
 const countModifier = (count = 0, action) => {
   if (action.type === `ADD`) {
     return count + 1;
@@ -16,9 +18,13 @@ const countModifier = (count = 0, action) => {
 
 const countStore = createStore(countModifier);
 
-countStore.dispatch({ type: `ADD` });
-countStore.dispatch({ type: `ADD` });
-countStore.dispatch({ type: `ADD` });
-countStore.dispatch({ type: `MINUS` });
+add.addEventListener("click", () => {
+  countStore.dispatch({ type: `ADD` });
+});
+minus.addEventListener("click", () => {
+  countStore.dispatch({ type: `MINUS` });
+});
 
-console.log(countStore.getState());
+countStore.subscribe(() => {
+  number.innerText = countStore.getState();
+});
