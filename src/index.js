a3d5ff1f8cb64b45f5953d394195b3f2d676d949
Yunsu <kenatman0723@gmail.com>
2021-06-07 16:39:34 +0900

    vanilla-redux : ADD_TODO : Never Mutate state

--- src/index.js
+++ src/index.js
@@ -5,17 +5,29 @@ const form = document.querySelector("form");
 const input = form.querySelector("input");
 const list = document.querySelector("ul");
 
-const displayTodo = (todo) => {
-  const li = document.createElement("li");
-  li.innerText = todo;
-  list.appendChild(li);
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [...state, { text: action.text, id: Date.now() }];
+    case DELETE_TODO:
+      return [];
+    default:
+      return state;
+  }
 };
+
+const store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
 
 const handleSubmit = (e) => {
   e.preventDefault();
   const todo = input.value;
-  displayTodo(todo);
   input.value = ``;
+  store.dispatch({ type: ADD_TODO, text: todo });
 };
 
 form.addEventListener("submit", handleSubmit);
